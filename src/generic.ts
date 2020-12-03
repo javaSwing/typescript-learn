1e6730ebc7e4708afcdc3d1417cc0b254a43a340
javaSwing <zxdlovejava@gmail.com>
2020-12-03 23:31:28 +0800

    complete generic

---
+++ src/generic.ts
@@ -0,0 +1,110 @@
+// TS中的泛型
+
+// 1. 泛型之Hello World
+// function identity (arg: any) {
+//   return arg
+// }
+
+// 泛型版本函数
+// function identity<T> (arg: T): T {
+//   return arg;
+// }
+
+// 第一种使用方法在函数之后使用<>中传入指定的类型
+// let res = identity<string>('javaswing');
+// console.log(res) // javaswing
+
+// 第二种利用类型推论-编译器会根据传入的参数自动帮助我们确定T的类型
+// let re = identity(2);
+// console.log(re)
+
+// 2. 使用泛型变量,使用泛型做为一个变量进行变成为其它类型
+
+// function loggerIdentity<T>(arg: T[]) {
+//   console.log(arg.length)
+//   return arg
+// }
+
+
+// loggerIdentity([2, 2, 2])
+
+// 3. 泛型类型
+
+// function identity<T> (arg: T): T {
+//   return arg;
+// }
+
+// let myIdentity: <T>(arg: T) => T = identity; // 泛型函数，与正常的函数描述前面多加了一个<T>
+
+
+// interface GenericIdentityFn<T> {
+//   (arg: T): T
+// }
+
+// 4. 泛型类
+
+// class GenericNumber<T> {
+//   zeroValue: T;
+//   add: (x: T, y: T) => T;
+// }
+
+// 5. 泛型约束
+
+interface Lengthwise {
+  length: number;
+}
+
+function identity<T extends Lengthwise>(arg: T) {
+  console.log(arg.length)
+  return arg
+}
+
+// identity(2); // Argument of type 'number' is not assignable to parameter of type 'Lengthwise'
+
+// identity({length: 1, value: 2}); // 传入符合指定约束的类型值
+
+// 6. 在泛型类型中使用类型参数
+
+// function getProperty<T, K extends keyof T>(obj: T, key: K) {
+//   return obj[key]
+// }
+
+// let ob ={name: 'javaswing', age: 28}
+
+// console.log(getProperty(ob, "age")); // 28
+// console.log(getProperty(ob, "name")); // javaswing
+
+// 7. 在泛型中使用类类型
+
+function create<T>(c: new () => T): T{
+  return new c()
+}
+
+// 使用原型属性推断并约束构造函数与类实例的关系
+class BeeKeepr {
+  hasMark: boolean;
+}
+
+class ZooKeeper {
+  nametag: string;
+}
+
+class Animal {
+  numLegs: number;
+}
+
+
+class Bee extends Animal {
+  keeper: BeeKeepr;
+}
+
+class Loin extends Animal {
+  keeper: ZooKeeper;
+}
+
+function createInstance<T extends Animal>(c: {new (): T}):T {
+  return new c();
+}
+
+createInstance(Loin).keeper.nametag;
+createInstance(Bee).keeper.hasMark;
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -3,14 +3,28 @@
 
 // 1.1 interface用来描述参数
 
-interface LablededValue {
-  label: string;
+// interface LablededValue {
+//   label: string;
+// }
+
+// function printLabel(labeledObj: LablededValue) {
+//   console.log(labeledObj.label)
+// }
+
+// let myObj = { size: 10, label: 'size 10 label' };
+
+// printLabel(myObj)
+
+
+// 函数类型
+
+// 函数类型分为：函数声明（Funtion Declaration）和函数表达式（Function Expression）
+
+interface SearchFunc { // 此interface描述为，函数表达式方式，:的左边为函数的输入，右边为函数的输出。
+  (source: string, subString: string): boolean;
 }
 
-function printLabel(labeledObj: LablededValue) {
-  console.log(labeledObj.label)
+// 在TS中的 => 用来表示函数的定义，不要和ES6中箭头函数搞混了。其中 => 左侧为输入类型，右侧为输出类型。
+let mySum: (x: number, y: number) => number = function (x:number, y:number):number {
+  return x + y
 }
-
-let myObj = { size: 10, label: 'size 10 label' };
-
-printLabel(myObj)
