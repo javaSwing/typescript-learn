1bf8f80ede8086d30bd200fae883c5fc90fb9d3f
javaSwing <zxdlovejava@gmail.com>
2020-12-10 22:19:36 +0800

    📦 feat: 符串字面量类型

---
+++ src/literal-types.ts
@@ -0,0 +1,11 @@
+// 字面量类型 (Literal Types)
+
+// 字符串字面量
+
+let foo: 'Hello'; // 将一个字符串做为一个类型
+
+type Direction = 'North' | 'East' | 'South' | 'West'; // 字符串字面量
+
+type Bools = true | false;
+
+type titleSize = 8 | 16 | 32;
